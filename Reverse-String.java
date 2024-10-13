42879ecbd4c80e1389d608bfbb27b5c1b3e3e586
Vaibhav Sorcar <120783798+vaibhavsorcar@users.noreply.github.com>
2024-10-13 16:54:41 +0530

    Reverse-String to Reverse-String.java

--- Reverse-String.java
+++ Reverse-String.java
@@ -3,12 +3,11 @@ class Test {
     public static void main(String[] args) {
         String s = "great for interview preparation";
         System.out.println("Original: " + s);
-
         Stack<String> st = new Stack<>();
         for (String word : s.split(" ")) {
             st.push(word);
         }
-
+        System.out.println(st);
         StringBuilder reversed = new StringBuilder();
         while (!st.isEmpty()) {
             reversed.append(st.pop()).append(st.isEmpty() ? "" : " ");
